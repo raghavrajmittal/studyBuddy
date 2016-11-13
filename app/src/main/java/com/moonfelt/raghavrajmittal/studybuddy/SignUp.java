9e7eaa90abe2b0a28496dbfaa3434800deac8c49
raghavrajmittal <raj.raghav@gmail.com>
2016-11-13 05:45:51 -0500

    Sign up screen connection

---
+++ app/src/main/java/com/moonfelt/raghavrajmittal/studybuddy/SignUp.java
@@ -0,0 +1,45 @@
+package com.moonfelt.raghavrajmittal.studybuddy;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.widget.ButtonBarLayout;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBar.Tab;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.widget.TextView;
+
+public class SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        //Intent intent = getIntent();
+        //String message = intent.getStringExtra(HomePage.EXTRA_MESSAGE);
+        //TextView textView = new TextView(this);
+        //textView.setTextSize(40);
+        //textView.setText(message);
+
+        //ViewGroup layout = (ViewGroup) findViewById(R.id.activity_buddy);
+        //layout.addView(textView);
+    }
+
+
+}
+
